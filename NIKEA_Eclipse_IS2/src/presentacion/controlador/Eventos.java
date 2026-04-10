2439c6fa299ce31fd902c09aec18c7bdb0db6d1a
inespe10 <inespe10@ucm.es>
2026-04-10 13:50:45 +0200

    Update Eventos.java

--- NIKEA_Eclipse_IS2/src/presentacion/controlador/Eventos.java
+++ NIKEA_Eclipse_IS2/src/presentacion/controlador/Eventos.java
@@ -2,8 +2,8 @@ package presentacion.controlador;
 
 public class Eventos {
 
-	public static final int ALTA_CLIENTE = 1;
-    public static final int BAJA_CLIENTE = 2;
-    public static final int MODIFICAR_CLIENTE = 3;
+	public static final int ALTA_CLIENTE = 101;
+    public static final int BAJA_CLIENTE = 102;
+    public static final int MODIFICAR_CLIENTE = 103;
     
 }
