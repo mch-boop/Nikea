37b5bc86d5b154634f98fb768ec6d6b6734d703d
Andy-uno <andgar47@ucm.es>
2026-04-24 21:03:50 +0200

    Completado GUI de clases y homogeneizado vista y arreglado GUICliente

--- NIKEA_Eclipse_IS2/src/presentacion/controlador/Eventos.java
+++ NIKEA_Eclipse_IS2/src/presentacion/controlador/Eventos.java
@@ -33,9 +33,6 @@ public class Eventos {
     public static final int MODIFICAR_BUSCAR_CLIENTE = 107;
     public static final int RES_MODIFICAR_BUSCAR_CLIENTE_OK = 1071;
     public static final int RES_MODIFICAR_BUSCAR_CLIENTE_KO = 1070;
-
-    
-	// EVENTOS DE SERVICIO
 	
 	// EVENTOS DE EMPLEADO
     
@@ -93,6 +90,60 @@ public class Eventos {
 	
 	// EVENTOS DE MARCA
 	
-	
+    public static final int ALTA_MARCA = 501;
+    public static final int RES_ALTA_MARCA_OK = 5011;
+    public static final int RES_ALTA_MARCA_KO = 5010;
+    public static final int RES_ALTA_MARCA_YA_EXISTE = 50105;
+
+    public static final int BAJA_MARCA = 502;
+    public static final int RES_BAJA_MARCA_OK = 5021;
+    public static final int RES_BAJA_MARCA_KO = 5020;
+    public static final int RES_BAJA_MARCA_KO_NO_EXISTE = 50203;
+
+    public static final int MODIFICAR_MARCA = 503;
+    public static final int RES_MODIFICAR_MARCA_OK = 5031;
+    public static final int RES_MODIFICAR_MARCA_KO = 5030;
+
+    public static final int BUSCAR_MARCA = 504;
+    public static final int RES_BUSCAR_MARCA_OK = 5041;
+    public static final int RES_BUSCAR_MARCA_KO = 5040;
+
+    public static final int MOSTRAR_MARCAS = 505;
+    public static final int RES_MOSTRAR_MARCAS_OK = 5051;
+    public static final int RES_MOSTRAR_MARCAS_KO = 5050;
+
+    public static final int MOSTRAR_RANKING_MARCA = 506;
+    public static final int RES_MOSTRAR_RANKING_MARCA_OK = 5061;
+    public static final int RES_MOSTRAR_RANKING_MARCA_KO = 5060;
+    
+    // EVENTOS DE SERVICIO
+    
+    public static final int ALTA_SERVICIO = 601;
+    public static final int RES_ALTA_SERVICIO_OK = 6011;
+    public static final int RES_ALTA_SERVICIO_KO = 6010;
+    public static final int RES_ALTA_SERVICIO_YA_EXISTE = 60105;
+
+    public static final int BAJA_SERVICIO = 602;
+    public static final int RES_BAJA_SERVICIO_OK = 6021;
+    public static final int RES_BAJA_SERVICIO_KO = 6020;
+    public static final int RES_BAJA_SERVICIO_KO_NO_EXISTE = 60203;
+
+    public static final int MODIFICAR_SERVICIO = 603;
+    public static final int RES_MODIFICAR_SERVICIO_OK = 6031;
+    public static final int RES_MODIFICAR_SERVICIO_KO = 6030;
+
+    public static final int BUSCAR_SERVICIO = 604;
+    public static final int RES_BUSCAR_SERVICIO_OK = 6041;
+    public static final int RES_BUSCAR_SERVICIO_KO = 6040;
+
+    public static final int MOSTRAR_SERVICIOS = 605;
+    public static final int RES_MOSTRAR_SERVICIOS_OK = 6051;
+    public static final int RES_MOSTRAR_SERVICIOS_KO = 6050;
+
+    public static final int MOSTRAR_MEJOR_ARTICULO = 606;
+    public static final int RES_MOSTRAR_MEJOR_ARTICULO_OK = 6061;
+    
+    public static final int ORGANIZAR_MONTAJE = 607;
+    public static final int RES_ORGANIZAR_MONTAJE_OK = 6071;
     
 }
