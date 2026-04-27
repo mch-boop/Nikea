bf87871c11f825c7fe1e7f3ff6331fd8cd6a4f9c
mch-boop <mcheikho@ucm.es>
2026-04-27 11:13:05 +0200

    solucion errores iniciales

--- NIKEA_Eclipse_IS2/src/negocio/descuento/TDescuento.java
+++ NIKEA_Eclipse_IS2/src/negocio/descuento/TDescuento.java
@@ -4,10 +4,25 @@ public class TDescuento {
 	
 	// ATRIBUTOS
 	
+	public TDescuento(boolean importe) {
+		// TODO Auto-generated constructor stub
+	}
 	private int idDescuento;
 	private String nombre;
 	private String porcentaje;
 	private boolean activo;
+	public void setCodigo(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setPorcentaje(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setDescripcion(String text) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	// MÉTODOS
 	
