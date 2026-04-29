f33994595d8d04b0d05bc6275405f53f153715b7
mch-boop <mcheikho@ucm.es>
2026-04-29 14:32:03 +0200

    Cambios para relacion montador-montaje

--- NIKEA_Eclipse_IS2/src/negocio/factura/SAFactura.java
+++ NIKEA_Eclipse_IS2/src/negocio/factura/SAFactura.java
@@ -2,9 +2,11 @@ package negocio.factura;
 
 import java.util.List;
 
+import negocio.servicio.TServicio;
+
 public interface SAFactura {
 	public boolean iniciarVenta(TFactura factura);
-
+	public boolean añadirLinea(TLineaFactura linea, TServicio servicio, int idMontador);
     public int cerrarVenta(TFactura factura);
 
     public List<TFactura> mostrarTodas();
--- NIKEA_Eclipse_IS2/src/negocio/factura/SAFacturaImp.java
+++ NIKEA_Eclipse_IS2/src/negocio/factura/SAFacturaImp.java
@@ -2,15 +2,24 @@ package negocio.factura;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import integracion.factoria.FactoriaAbstractaIntegracion;
 import integracion.factura.DAOFactura;
 import integracion.factura.DAOLineaFactura;
+import integracion.empleado.DAOEmpleado;
+import integracion.empleado.DAOMontadorMontaje;
+import negocio.empleado.TEmpleado;
+import negocio.empleado.TMontador;
+import negocio.empleado.TMontadorMontaje;
+import negocio.servicio.TServicio;
 
 public class SAFacturaImp implements SAFactura {
-
+	private DAOMontadorMontaje daoMontaje = FactoriaAbstractaIntegracion.getInstance().crearDAOMontadorMontaje();
 	private TFactura facturaActual;
-
+	private Map<Integer, Integer> servicioAMontador = new HashMap<>();
+    private DAOEmpleado daoEmpleado =
+        FactoriaAbstractaIntegracion.getInstance().crearDAOEmpleado();
 	@Override
 	public boolean iniciarVenta(TFactura factura) {
 
@@ -22,9 +31,65 @@ public class SAFacturaImp implements SAFactura {
 		facturaActual.setLineas(new ArrayList<>());
 		facturaActual.setCerrada(false);
 
+		servicioAMontador.clear();
 		return true;
 	}
+	
+	//Está hecha la lógica para vincular y desvincular montadores, queda el resto.
+	@Override
+	public boolean añadirLinea(TLineaFactura linea, TServicio servicio, int idMontador) {
+		if (facturaActual == null || linea == null || servicio == null)
+	        return false;
 
+	    facturaActual.getLineas().add(linea);
+
+	    //if (esMontaje(servicio)) {
+
+            Integer idMontador = obtenerMontadorDisponible();
+
+            if (idMontador == null)
+                return false;
+
+            servicioAMontador.put(servicio.getId(), idMontador);
+
+            TMontadorMontaje tm = new TMontadorMontaje(
+                idMontador,
+                servicio.getId()
+            );
+
+            if (!daoMontaje.existeVinculacion(tm)) {
+                daoMontaje.vincular(tm);
+            }
+        //}
+		return true;
+		
+	}
+
+	public boolean eliminarLinea(TLineaFactura linea, TServicio servicio) {
+
+        if (facturaActual == null || linea == null || servicio == null)
+            return false;
+
+        facturaActual.getLineas().remove(linea);
+
+        if (esMontaje(servicio)) {
+
+            Integer idMontador = servicioAMontador.get(servicio.getId());
+
+            if (idMontador != null) {
+
+                TMontadorMontaje tm = new TMontadorMontaje(
+                    idMontador,
+                    servicio.getId()
+                );
+
+                daoMontaje.desvincular(tm);
+                servicioAMontador.remove(servicio.getId());
+            }
+        }
+
+        return true;
+    }
 	@Override
 	public int cerrarVenta(TFactura factura) {
 
@@ -55,7 +120,8 @@ public class SAFacturaImp implements SAFactura {
 		}
 
 		facturaActual = null;
-
+		servicioAMontador.clear();
+		
 		return id;
 	}
 
@@ -64,4 +130,22 @@ public class SAFacturaImp implements SAFactura {
 
 		return FactoriaAbstractaIntegracion.getInstance().crearDAOFactura().leerTodas();
 	}
+	
+	// Funciones Auxiliares
+	
+	private boolean esMontaje(TServicio servicio) {
+        return servicio.getTipo() != null && servicio.getTipo() == 2;
+    }
+
+    private Integer obtenerMontadorDisponible() {
+
+        for (TEmpleado e : daoEmpleado.readAll()) {
+
+            if (e.getTipo()== 2 && e.isActivo()) {
+                return e.getId();
+            }
+        }
+
+        return null;
+    }
 }
