a0bdd5a851976226cc354801aa83d93c4733ba09
Lukas Otero Asumendi <lucasote@ucm.es>
2026-04-30 20:12:07 +0200

    mostrarservicios boton cancelar

--- NIKEA_Eclipse_IS2/src/presentacion/GUIServicio/VistaMostrarServicios.java
+++ NIKEA_Eclipse_IS2/src/presentacion/GUIServicio/VistaMostrarServicios.java
@@ -63,7 +63,7 @@ public class VistaMostrarServicios extends JFrame implements IGUI {
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
         JPanel panelSur = new JPanel();
-        btnCancelar = new JButton("CANCELAR");
+        btnCancelar = new JButton("SALIR");
         panelSur.add(btnCancelar);
 
         btnCancelar.addActionListener(e -> {
