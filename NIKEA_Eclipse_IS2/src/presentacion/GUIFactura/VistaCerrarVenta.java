4db701bc83fdfe9db43b83ec1d0c25726381543e
mch-boop <mcheikho@ucm.es>
2026-04-30 14:21:17 +0200

    Spinner de fecha

--- NIKEA_Eclipse_IS2/src/presentacion/GUIFactura/VistaCerrarVenta.java
+++ NIKEA_Eclipse_IS2/src/presentacion/GUIFactura/VistaCerrarVenta.java
@@ -20,13 +20,20 @@ import presentacion.IGUI;
 import presentacion.controlador.Controlador;
 import presentacion.controlador.Eventos;
 import negocio.factura.TFactura;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.JSpinner.DateEditor;
+import java.util.Date;
+import java.util.Calendar;
 
 @SuppressWarnings("serial")
 public class VistaCerrarVenta extends JFrame implements IGUI {
 
 	// ATRIBUTOS
-	private JTextField txtIdCliente, txtIdDescuento, txtFecha;
+	private JTextField txtIdCliente, txtIdDescuento;
 	private JButton btnAceptar, btnCancelar;
+	private JSpinner spinnerDia, spinnerMes, spinnerAnyo;
 
 	// CONSTRUCTORA
 	public VistaCerrarVenta() {
@@ -38,7 +45,6 @@ public class VistaCerrarVenta extends JFrame implements IGUI {
 	private void limpiarCampos() {
 		txtIdCliente.setText("");
 		txtIdDescuento.setText("");
-		txtFecha.setText("");
 	}
 
 	// INIT GUI
@@ -51,7 +57,6 @@ public class VistaCerrarVenta extends JFrame implements IGUI {
 		// Campos
 		txtIdCliente = new JTextField(20);
 		txtIdDescuento = new JTextField(20);
-		txtFecha = new JTextField(20);
 
 		// Panel botones
 		JPanel pBotones = new JPanel();
@@ -61,6 +66,18 @@ public class VistaCerrarVenta extends JFrame implements IGUI {
 		pBotones.add(btnAceptar);
 		pBotones.add(btnCancelar);
 
+		//Spinners
+		int anioActual = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
+		spinnerAnyo = new JSpinner(new SpinnerNumberModel(anioActual, 1900, anioActual, 1));
+		
+		int diaActual = java.util.Calendar.getInstance().get(java.util.Calendar.DAY_OF_MONTH);
+		spinnerDia = new JSpinner(new SpinnerNumberModel(diaActual, 1, 31, 1));
+
+		int mesActual = java.util.Calendar.getInstance().get(java.util.Calendar.MONTH);
+		spinnerMes = new JSpinner(new SpinnerNumberModel(mesActual+1, 1, 12, 1));
+
+		
+		
 		// Acción aceptar
 		btnAceptar.addActionListener(e -> {
 			try {
@@ -68,7 +85,15 @@ public class VistaCerrarVenta extends JFrame implements IGUI {
 
 				tFactura.setIdCliente(Integer.parseInt(txtIdCliente.getText()));
 				tFactura.setIdDescuento(Integer.parseInt(txtIdDescuento.getText()));
-				tFactura.setFecha(txtFecha.getText());
+				
+				int dia = (Integer) spinnerDia.getValue();
+				int mes = (Integer) spinnerMes.getValue();
+				int anio = (Integer) spinnerAnyo.getValue();
+
+				// Formato string
+				String fecha = String.format("%02d/%02d/%04d", dia, mes, anio);
+
+				tFactura.setFecha(fecha);
 
 				Controlador.getInstance().accion(Eventos.CERRAR_VENTA, tFactura);
 
@@ -111,8 +136,17 @@ public class VistaCerrarVenta extends JFrame implements IGUI {
 		ajuste.gridx = 0;
 		ajuste.gridy = 2;
 		formPanel.add(new JLabel("Fecha:"), ajuste);
+
 		ajuste.gridx = 1;
-		formPanel.add(txtFecha, ajuste);
+
+		JPanel panelFecha = new JPanel();
+		panelFecha.add(spinnerDia);
+		panelFecha.add(new JLabel("/"));
+		panelFecha.add(spinnerMes);
+		panelFecha.add(new JLabel("/"));
+		panelFecha.add(spinnerAnyo);
+
+		formPanel.add(panelFecha, ajuste);
 
 		// Título
 		JLabel lblTitulo = new JLabel("Introduzca los datos para cerrar la venta:");
