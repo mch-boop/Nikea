1eeaeaf3c6e885d71cb75e0447977853740ebec4
DavidMoralesGit <dmoral12@ucm.es>
2026-05-08 01:48:20 +0200

    Quito interfaz toa

--- NIKEA_Eclipse_IS2/src/negocio/operacionTOA/TResumenNegocio.java
+++ NIKEA_Eclipse_IS2/src/negocio/operacionTOA/TResumenNegocio.java
@@ -1,9 +1,42 @@
 package negocio.operacionTOA;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
-public interface TResumenNegocio {
-	public int getTotalClientes();
-    public int getTotalServicios();
-    public int getTotalFacturas();
-    public int getTotalMarcas();
+import negocio.cliente.TCliente;
+import negocio.factura.TFactura;
+import negocio.servicio.TServicio;
+
+public class TResumenNegocio {
+	private Collection<TCliente> clientes;
+	private Collection<TServicio> servicios;
+	private List<TFactura> facturas;
+	private Collection<String> marcas;
+
+	public TResumenNegocio(Collection<TCliente> clientes, Collection<TServicio> servicios, List<TFactura> facturas,
+			Set<String> marcas2) {
+		this.clientes = clientes;
+		this.servicios = servicios;
+		this.facturas = facturas;
+		this.marcas = marcas2;
+	}
+
+	// getters
+	 public int getTotalClientes() {
+        return clientes != null ? clientes.size() : 0;
+    }
+
+	public int getTotalServicios() {
+        return servicios != null ? servicios.size() : 0;
+    }
+
+	public int getTotalFacturas() {
+        return facturas != null ? facturas.size() : 0;
+    }
+
+    public int getTotalMarcas() {
+        return marcas != null ? marcas.size() : 0;
+    }
+	
 }
